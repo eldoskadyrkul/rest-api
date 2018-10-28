e6b89d172769c8d0c17d5489fb8c60a19fd6a627
eldoskadyrkul <41547811+eldoskadyrkul@users.noreply.github.com>
2018-10-28 13:06:26 +0600

    initial commit

--- src/main/java/com/example/restapi/controller/ChangeController.java
+++ src/main/java/com/example/restapi/controller/ChangeController.java
@@ -4,11 +4,9 @@ import com.example.restapi.model.UserModel;
 import com.example.restapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class ChangeController {
